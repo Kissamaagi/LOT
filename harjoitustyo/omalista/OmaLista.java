7297831f15ddf5715296004668812363057fb767
Miia Pynnönen <miia.pynnonen@tuni.fi>
2020-04-16 22:09:17 +0300

    OmaListan lisää metodia tehty

--- harjoitustyo/omalista/OmaLista.java
+++ harjoitustyo/omalista/OmaLista.java
@@ -12,11 +12,29 @@ import java.util.LinkedList;
  * @author Miia
  */
 public class OmaLista<E> extends LinkedList<E> implements Ooperoiva<E> {
+
     @Override
     @SuppressWarnings({"unchecked"})
     public void lisää(E uusi) throws IllegalArgumentException{
         if (uusi != null) {
-            this.add(uusi);
+            if (this.size() == 0) {
+                this.add(uusi);
+            } 
+            else {
+                for(int i = 0; i < this.size(); i++) {
+                    E temp = this.get(i);
+                    Comparable compTemp = (Comparable)temp;
+                    Comparable compUusi = (Comparable)uusi;
+
+                    //NÄÄ EI TOIMI, EI LISÄÄ OIKEIN, LOGIIKKAONKLEMA VISSII
+                    if (compUusi.compareTo(compTemp) == -1) {
+                        this.add(i, uusi);
+                    }
+                    else if (compUusi.compareTo(compTemp) == 0) {
+                        this.add(i+1, uusi);
+                    }
+                }
+            }
         }
         else {
             throw new IllegalArgumentException();
