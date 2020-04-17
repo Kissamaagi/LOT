e5e652b63dc59340c9894f1cf0a94944dce3fb87
Miia Pynnönen <miia.pynnonen@tuni.fi>
2020-04-17 18:55:51 +0300

    OmaLista-luokan lisää-metodi tehty valmiiksi

--- harjoitustyo/omalista/OmaLista.java
+++ harjoitustyo/omalista/OmaLista.java
@@ -16,44 +16,25 @@ public class OmaLista<E> extends LinkedList<E> implements Ooperoiva<E> {
     @Override
     @SuppressWarnings({"unchecked"})
     public void lisää(E uusi) throws IllegalArgumentException{
-        if (uusi != null) {
+        if (uusi != null && uusi instanceof Comparable) {
+            //Jos lista on tyhjä, lisätään uusi alkio suoraan
             if (this.size() == 0) {
                 this.add(uusi);
-                System.out.println("uusi lisätty: "+0);
             } 
             else {
                 Comparable compUusi = (Comparable)uusi;
 
+                //Käydään lista kohta kohdalta läpi
                 for(int i = 0; i < this.size(); i++) {
                     Comparable compTemp = (Comparable)this.get(i);
                     
-                    //NYT EHKÄ TOIMII???   TAI EI??? 
+                    //Jos uusi alkio on pienempi kuin kyseisen kohdan alkio, lisätään se sen paikalle
                     if (compTemp.compareTo(compUusi) >= 1) {
-                        System.out.println("uusi on isompi: "+i);
                         this.add(i, uusi);
                         break;
                     }
-                    else if (compTemp.compareTo(compUusi) < 0) {
-                        System.out.println("uusi on yhtäsuuri: "+i);
-                        this.add(i+1, uusi);
-                        break;
-                    }
-                    /*
-                    *else if (compTemp.compareTo(compUusi) == 0) {
-                    *    System.out.println("Yhtäsuuri...");
-                    *    for(int j = i; j < this.size(); j++) {
-                    *        Comparable compYhtäsuuri = (Comparable)this.get(j);
-                    *        System.out.println(j);
-                    *        if(compYhtäsuuri.compareTo(compUusi) > 0) {
-                    *            System.out.println("uusi on samankokoinen: "+j);
-                    *            this.add(j, uusi);
-                    *            break;
-                    *        }  
-                    *    }     
-                    *}
-                    */
+                    //Jos uusi alkio on suurempi kuin muut alkiot listalla, lisätään se listan viimeiseksi
                     else if (i == this.size()-1) {
-                        System.out.println("uusi on kaikkia pienempi, menee loppuun: "+i+1);
                         this.addLast(uusi);
                         break;
                     }
