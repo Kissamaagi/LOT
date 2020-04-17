942345b07a40107a2d862cd46b396169cde6c1f5
Miia Pynnönen <miia.pynnonen@tuni.fi>
2020-04-17 19:45:40 +0300

    Kokoelma-luokka tehty

--- harjoitustyo/kokoelma/Kokoelma.java
+++ harjoitustyo/kokoelma/Kokoelma.java
@@ -5,10 +5,42 @@
  */
 package harjoitustyo.kokoelma;
 
+import harjoitustyo.omalista.OmaLista;
+import harjoitustyo.dokumentit.*;
+import harjoitustyo.apulaiset.Kokoava;
 /**
  *
  * @author Miia
  */
-public class Kokoelma {
+public class Kokoelma extends Object implements Kokoava<Dokumentti> {
+    private OmaLista<Dokumentti> dokumentit;
+
+    public Kokoelma() {
+        dokumentit = new OmaLista<Dokumentti>();
+    }
+
+    public OmaLista<Dokumentti> dokumentit() {
+        return dokumentit;
+    }
+
+    public void lisää(Dokumentti uusi) throws IllegalArgumentException {
+        if (uusi != null && !dokumentit.contains(uusi)) {
+            dokumentit.lisää(uusi);
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public Dokumentti hae(int tunniste) {
+        if (dokumentit.size() > 0) {
+            for (Dokumentti dokumentti : dokumentit) {
+                if (tunniste == dokumentti.tunniste()) {
+                    return dokumentti;
+                }
+            }
+        }
+        return null;
+    }
     
 }
