81f225e787253d88eec81620ba0c31b00ed58055
Miia Pynnönen <miia.pynnonen@tuni.fi>
2020-05-06 21:39:43 +0300

    Käyttöliittymää jatkettu

--- harjoitustyo/Kayttoliittyma.java
+++ harjoitustyo/Kayttoliittyma.java
@@ -3,13 +3,15 @@ package harjoitustyo;
 import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 import harjoitustyo.dokumentit.Dokumentti;
 import harjoitustyo.dokumentit.Uutinen;
 import harjoitustyo.dokumentit.Vitsi;
 import harjoitustyo.kokoelma.Kokoelma;
 import java.util.LinkedList;
 import java.util.Scanner;
+
+//TODO: polish, add, remove, find
+//TODO: add-komento: dokumentin tyypin tarkistus, lisääminen 
 
 /**
  * Harjoitustyön Käyttöliittymä-luokka. Luokassa tapahtuu kaikki interaktio
@@ -83,11 +85,40 @@ public class Kayttoliittyma {
             System.out.println("Missing file!");
         }
         else {
+            lisääKokoelmaan(dokTiedosto);
+            luoSulkusanaLista(sulkuTiedosto);
+            Scanner inputReader = new Scanner(System.in);
+            boolean echo = false;
             while (jatkuu) {
                 System.out.println("Please, enter a command:");
-                jatkuu = false;
+                String[] komennot = inputReader.nextLine().split(" ");
+
+                if (echo) {
+                    System.out.println(komennot[0]);
+                }
+
+                switch (komennot[0]) {
+                    case "quit": jatkuu = false;
+                                 break;
+                    case "echo": echo = !echo; 
+                                 break;
+                    case "print": tulosta(komennot);
+                                  break;
+                    case "reset": lisääKokoelmaan(dokTiedosto);
+                                  luoSulkusanaLista(sulkuTiedosto);
+                                  break;
+                    case "add": lisääKokoelmaan(komennot);
+                                break;
+                    case "remove":
+                    case "find":
+                    case "polish":
+                    default: System.out.println("Error!");
+                             break;
+                }
             }
+            inputReader.close();
         }
+        
         System.out.println("Program terminated.");
     }
 
@@ -126,6 +157,22 @@ public class Kayttoliittyma {
     }
 
     /**
+     * Metodi käyttöliittymän kautta uuden dokumentin kokoelmaan lisäämiseen.
+     * 
+     * @param komennot käyttäjän antamat komennot, joiden mukaan dokumentti lisätään.
+     */
+    public void lisääKokoelmaan(String[] komennot) {
+        if (komennot.length >= 2) {
+            //selvitetään eka onko uutinen vai vitsi
+
+            //sitten onkoUutinen booleanin mukaan lisätään
+        }
+        else {
+            System.out.println("Error!");
+        }    
+    }
+
+    /**
      * Metodissa lisätään tiedostosta sulkusanat sulkusanalistaan
      * 
      * @param sulkuTiedosto sulkusanat sisältävä tiedosto
@@ -143,6 +190,54 @@ public class Kayttoliittyma {
         }
         catch (Exception e) {
             sulkuLukija.close();
+        }
+    }
+
+    /**
+     * Metodi tulostaa komennon mukaan joko yhden dokumentin tunnisteen mukaan,
+     * tai kaikki kokoelman dokumentit jos tunnistetta ei ole annettu.
+     * 
+     * @param komennot joista päätellään mitä tulostetaan
+     */
+    public void tulosta(String[] komennot) {
+        if (komennot.length == 1) {
+            for (Dokumentti dokumentti : kokoelma.dokumentit()) {
+                System.out.println(dokumentti.toString());
+            }
+        }
+        else if (komennot.length == 2) {
+            int haettavaTunniste = onkoNumero(komennot[1]);
+
+            if (haettavaTunniste > 0) {
+                Dokumentti tulostettava = kokoelma.hae(haettavaTunniste);
+                if (tulostettava != null) {
+                    System.out.println(tulostettava.toString());
+                }
+                else {
+                    System.out.println("Error!");
+                }
+            }
+            else {
+                System.out.println("Error!");
+            }
+        }
+
+    }
+
+    /**
+     * Metodi, jolla selvitetään sisältääkö String-tyyppinen
+     * muuttuja numeron vai ei.
+     * 
+     * @param ehkäNumero String joka tarkistetaan.
+     * @return int, joka vastaa Stringissä olevaa numeroa 
+     * tai null, jos numeroa ei löytynyt.
+     */
+    public int onkoNumero(String ehkäNumero) {
+        try {
+            return Integer.parseInt(ehkäNumero);
+        }
+        catch (Exception e) {
+            return 0;
         }
     }
 }
